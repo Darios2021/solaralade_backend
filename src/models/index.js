8ea84777eed29eb50a88a2de15ac31a2df0bdb18
Darios2021 <perezruartedario@gmail.com>
2025-11-17 15:17:37 -0300

    asd1

--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,34 @@
 // src/models/index.js
 const Lead = require('./Lead')
+const ChatSession = require('./ChatSession')
+const ChatMessage = require('./ChatMessage')
+
+// =======================
+// Relaciones
+// =======================
+
+// Lead → ChatSession (un lead puede tener muchos chats)
+Lead.hasMany(ChatSession, {
+  foreignKey: 'leadId',
+  as: 'chatSessions',
+})
+ChatSession.belongsTo(Lead, {
+  foreignKey: 'leadId',
+  as: 'lead',
+})
+
+// ChatSession ↔ ChatMessage (una sesión tiene muchos mensajes)
+ChatSession.hasMany(ChatMessage, {
+  foreignKey: 'sessionId',
+  as: 'messages',
+})
+ChatMessage.belongsTo(ChatSession, {
+  foreignKey: 'sessionId',
+  as: 'session',
+})
 
 module.exports = {
   Lead,
+  ChatSession,
+  ChatMessage,
 }
